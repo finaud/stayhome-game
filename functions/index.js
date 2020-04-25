d3fa3f60919d71a02dceaaaffea46d87e2351204
Antoine Finot <antoine.finot@mail.utoronto.ca>
2020-04-25 00:44:00 -0700

    removed auth

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,7 @@ exports.addMessage = functions.https.onCall((data, context) => {
 
     // Message text passed from the client.
     const text = data.text;
+    const uid = data.uid;
 
     // Checking attribute.
     if (!(typeof text === 'string') || text.length === 0) {
@@ -18,21 +19,12 @@ exports.addMessage = functions.https.onCall((data, context) => {
         throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
             'one arguments "text" containing the message text to add.');
     }
-    // Checking that the user is authenticated.
-    if (!context.auth) {
-        // Throwing an HttpsError so that the client gets the error details.
-        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
-            'while authenticated.');
-    }
-
-    // Authentication / user information is automatically added to the request.
-    const uid = context.auth.uid || null;
 
     // Saving the new message to the Realtime Database.
     const sanitizedMessage = "sanitized: " + text; // Sanitize the message.
     return db.collection('cities').doc('LA').set({
         text: sanitizedMessage,
-        author: { uid },
+        author: uid,
     }).then(() => {
         console.log('New Message written');
         // Returning the sanitized message to the client.
