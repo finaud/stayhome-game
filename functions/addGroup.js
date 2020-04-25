c1b042ddcaee18a8db42509b9041b554608c7005
Antoine Finot <antoine.finot@mail.utoronto.ca>
2020-04-25 12:08:19 -0700

    added buyIn to doc creation in addGroup function

--- functions/addGroup.js
+++ functions/addGroup.js
@@ -13,7 +13,7 @@ const addGroup = (data, context) => {
     const numWinners = parseInt(data.numWinners);
 
     return db.collection('groups').doc(name).set({
-        name, host, limit, numWinners, status: "PENDING"
+        name, host, limit, numWinners, buyIn, status: "PENDING"
     }).then(() => {
         console.log('New group created: ' + name);
         // Returning the sanitized message to the client.
