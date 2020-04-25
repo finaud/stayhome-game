363d07f36d7db584c57394e7fbfec4694194b17b
Antoine Finot <antoine.finot@mail.utoronto.ca>
2020-04-25 05:35:56 -0700

    added getFriends function

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,14 @@ const addMessage = require('./addMessage');
 const addUser = require('./addUser');
 const addGroup = require('./addGroup');
 const addFriend = require('./addFriend');
+const getFriends = require('./getFriends');
+
 
 module.exports = {
     'addMessage': functions.https.onCall(addMessage),
     'addUser': functions.https.onCall(addUser),
     'addGroup': functions.https.onCall(addGroup),
     'addFriend': functions.https.onCall(addFriend),
+    'getFriends': functions.https.onCall(getFriends),
+
 };
